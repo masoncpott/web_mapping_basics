604bca500cdb4f7c9d179f91dd3ff89a7df839bd
Mason Pott <mason.pott@slalom.com>
2022-06-07 20:45:30 -0700

    added population data

--- map1.py
+++ map1.py
@@ -13,17 +13,39 @@ folium.Marker([45.47001152268444, -122.61397957233314], popup="<i>My House New</
 ### process the data from the txt file ###
 volcano_data = pandas.read_csv("Webmap_datasources/Volcanoes.txt")
 
-latitude = list(volcano_data["LAT"])
-longitude = list(volcano_data["LON"])
+latitudes = list(volcano_data["LAT"])
+longitudes = list(volcano_data["LON"])
+elevations = list(volcano_data["ELEV"])
+names = list(volcano_data["NAME"])
 
-# len(latitude) == len(longitude) # output = True
+
 
 ### add markers to map that represent each volcano ###
-for lat, lon in zip(latitude, longitude):
-    folium.Marker(location=[lat, lon]).add_to(fg)
+def icon_color(elevation):
+    if elevation <= 2000:
+        return "green"
+    if elevation > 2000 and elevation <= 3000:
+        return "orange"
+    if elevation > 3000:
+        return "red"
+
+html = """<h3>%s</h3>
+Elevation: %s m """
+
+
+for lat, lon, el, name in zip(latitudes, longitudes, elevations, names):
+    folium.CircleMarker(
+        location = [lat, lon], 
+        radius = 6, 
+        popup = html % (name, str(el)), 
+        color = 'grey',
+        fill_opacity=0.7, 
+        fill=True, 
+        fill_color=icon_color(el)
+        ).add_to(fg)
+
+fg.add_child(folium.GeoJson(data=open("Webmap_datasources/world.json", 'r', encoding="utf-8-sig").read()))
 
 map1.add_child(fg)
-
-
 
 map1.save("Map1.html")
